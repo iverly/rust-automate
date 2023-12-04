142ff4edabaaba748f6213c7f83c0d23572e66ca
iverly <github@iverly.net>
2023-12-04 17:42:04 +0100

    feat: add d rules
    Signed-off-by: iverly <github@iverly.net>

--- src/rules/rule_r.rs
+++ src/rules/rule_r.rs
@@ -5,6 +5,12 @@ use crate::parser::Token;
 use super::{Rule, RuleSet, RuleStep};
 
 // R -> rate <num> <num> <num> R | rate <num> <num> <num> D | rate <num> <num> <num> C | None
+// D -> delay <num> <num> <num> R | delay <num> <num> <num> D | delay <num> <num> <num> C | None
+
+// R is basically the same as C, but with a rate token instead of a delay token
+// so in the token definition, we can just have a single token for both
+// and then in the rule definition, we can have a single rule for both
+// this is what we're doing here with the token `options`
 pub fn construct_r() -> RuleSet {
     let base: Vec<RuleStep> = vec![
         RuleStep {
